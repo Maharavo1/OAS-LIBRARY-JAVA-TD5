c764147a341376bd1c0de70142428f1e4f5608a8
Maharavo1 <hei.maharavo@gmail.com>
2023-11-19 21:37:44 +0300

    feat: configuration contion database

---
+++ src/ConnectionConfig/ConnectionConfiguration.java
@@ -0,0 +1,27 @@
+package ConnectionConfig;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionConfiguration {
+    private String URL ;
+    private String PASS;
+    private String USER ;
+
+    public ConnectionConfiguration() {
+        this.URL = "jdbc:postgresql://localhost:5432/"+System.getenv("DB_URL");
+        this.USER = System.getenv("DB_USER");
+        this.PASS = System.getenv("DB_PASSWORD");
+    }
+    public Connection createConnection(){
+        try{
+            Connection connection = DriverManager.getConnection(
+                    this.URL , this.USER ,this.PASS
+            );
+            return connection ;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
